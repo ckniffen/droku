05d7eb7289eaa4d2870567dcef4c0bbc777144e1
cknif <c_kniffen@kniffenwebdesign.com>
2011-11-21 18:35:19 -0600

    Removed text spacers

--- gen/com/kniffenwebdesign/roku/R.java
+++ gen/com/kniffenwebdesign/roku/R.java
@@ -20,31 +20,27 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout01=0x7f060000;
-        public static final int TextView01=0x7f060014;
-        public static final int TextView02=0x7f060012;
         public static final int TextView03=0x7f060001;
         public static final int button_back=0x7f060003;
-        public static final int button_down=0x7f060013;
-        public static final int button_forward=0x7f060018;
+        public static final int button_down=0x7f060010;
+        public static final int button_forward=0x7f060014;
         public static final int button_home=0x7f060005;
-        public static final int button_info=0x7f06001b;
-        public static final int button_left=0x7f06000e;
-        public static final int button_play=0x7f060017;
-        public static final int button_replay=0x7f06001a;
-        public static final int button_reverse=0x7f060016;
-        public static final int button_right=0x7f060010;
-        public static final int button_select=0x7f06000f;
-        public static final int button_up=0x7f06000b;
-        public static final int linearLayout1=0x7f060019;
-        public static final int linearLayout2=0x7f060015;
+        public static final int button_info=0x7f060017;
+        public static final int button_left=0x7f06000c;
+        public static final int button_play=0x7f060013;
+        public static final int button_replay=0x7f060016;
+        public static final int button_reverse=0x7f060012;
+        public static final int button_right=0x7f06000e;
+        public static final int button_select=0x7f06000d;
+        public static final int button_up=0x7f06000a;
+        public static final int linearLayout1=0x7f060015;
+        public static final int linearLayout2=0x7f060011;
         public static final int linearLayout3=0x7f060002;
         public static final int linearLayout4=0x7f060006;
         public static final int tableLayout1=0x7f060008;
         public static final int tableRow1=0x7f060009;
-        public static final int tableRow2=0x7f06000d;
-        public static final int tableRow3=0x7f060011;
-        public static final int textView1=0x7f06000a;
-        public static final int textView2=0x7f06000c;
+        public static final int tableRow2=0x7f06000b;
+        public static final int tableRow3=0x7f06000f;
         public static final int textView3=0x7f060004;
         public static final int textView4=0x7f060007;
     }
