f706386886206d43e737a2f68f07b9f02eef6679
cknif <c_kniffen@kniffenwebdesign.com>
2011-11-29 18:57:24 -0600

    refactored ChannelAdapter

--- src/com/kniffenwebdesign/roku/ChannelAdapter.java
+++ src/com/kniffenwebdesign/roku/ChannelAdapter.java
@@ -7,6 +7,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -18,57 +19,60 @@ import android.widget.ImageView;
 
 import com.kniffenwebdesign.roku.ecp.Channel;
 import com.kniffenwebdesign.roku.ecp.EcpClient;
-import com.kniffenwebdesign.roku.ecp.Key;
 
 public class ChannelAdapter extends ArrayAdapter<Channel> {
 	private ArrayList<Channel> items;
 
-    public ChannelAdapter(Context context, int textViewResourceId, ArrayList<Channel> items) {
-            super(context, textViewResourceId, items);
-            this.items = items;
-    }
+	public ChannelAdapter(Context context, int textViewResourceId, ArrayList<Channel> items) {
+		super(context, textViewResourceId, items);
+		this.items = items;
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-            View v = convertView;
-            if (v == null) {
-                LayoutInflater vi = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                v = vi.inflate(R.layout.channel_list_item, null);
-            }
-            final Channel channel = items.get(position);
-            if (channel != null) {
-        		ImageView channelIcon = (ImageView)v.findViewById(R.id.channel_icon);
-        		
-        		channelIcon.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                    	EcpClient.getInstance().launchChannel(channel.getId());
-                    }
-                    
-               });
-        		
-				try {
-					Bitmap bitmap = drawableFromUrl(channel.getImageSrc());
-	        		channelIcon.setImageBitmap(bitmap);
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if (view == null) {
+			LayoutInflater vi = (LayoutInflater) this.getContext()
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = vi.inflate(R.layout.channel_list_item, null);
+		}
+		final Channel channel = items.get(position);
+		if (channel != null) {
+			ImageView channelIcon = (ImageView) view.findViewById(R.id.channel_icon);
+
+			channelIcon.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View view) {
+					EcpClient.getInstance().launchChannel(channel.getId());
+					Activity activity = (Activity) getContext();
+					activity.finish();
 				}
-            }
-            return v;
-    }
-    
-    Bitmap drawableFromUrl(String url) throws java.net.MalformedURLException, java.io.IOException {
-        Bitmap x;
 
-        HttpURLConnection connection = (HttpURLConnection)new URL(url) .openConnection();
+			});
 
-        connection.connect();
-        InputStream input = connection.getInputStream();
+			try {
+				Bitmap bitmap = drawableFromUrl(channel.getImageSrc());
+				channelIcon.setImageBitmap(bitmap);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return view;
+	}
 
-        x = BitmapFactory.decodeStream(input);
-        return x;
-    }
+	Bitmap drawableFromUrl(String url) throws java.net.MalformedURLException,
+			java.io.IOException {
+		Bitmap bitmap;
+
+		HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+
+		connection.connect();
+		InputStream input = connection.getInputStream();
+
+		bitmap = BitmapFactory.decodeStream(input);
+		return bitmap;
+	}
 }
