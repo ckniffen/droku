c4b8651437bd30490a7ba0598275e0f522b12797
cknif <c_kniffen@kniffenwebdesign.com>
2011-12-10 13:45:11 -0600

    Setup Vibrate on button press.

--- src/com/kniffenwebdesign/roku/RemoteButtonListener.java
+++ src/com/kniffenwebdesign/roku/RemoteButtonListener.java
@@ -1,17 +1,22 @@
 package com.kniffenwebdesign.roku;
 
+import android.content.Context;
+import android.os.Vibrator;
 import android.view.MotionEvent;
 import android.view.View;
 
 import com.kniffenwebdesign.roku.ecp.Key;
 
 public class RemoteButtonListener implements View.OnClickListener, View.OnTouchListener {
-	public void onClick(View v) {
-		Key key = (Key) v.getTag(R.id.key_type);
+	public void onClick(View view) {
+		Vibrator vibrator = (Vibrator) view.getContext().getSystemService(Context.VIBRATOR_SERVICE);
+		vibrator.vibrate(50);
+		
+		Key key = (Key) view.getTag(R.id.key_type);
    	 	new EcpAsyncTask().execute(key);
 	}
 
-	public boolean onTouch(View v, MotionEvent event) {
+	public boolean onTouch(View view, MotionEvent event) {
 		// TODO Auto-generated method stub
 		return false;
 	}
